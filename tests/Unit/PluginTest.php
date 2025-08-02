533b4f43a997ab068af67abbb6189a8d6edba537
Denis <korbakovd@gmail.com>
2025-08-03 02:07:33 +0300

    fix: rename use class

--- tests/Unit/PluginTest.php
+++ tests/Unit/PluginTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use DenisKorbakov\LaravelDataScribe\Plugin;
+use DenisKorbakov\LaravelDataScribe\LaravelDataBodyParam;
 use Illuminate\Routing\Route;
 use Knuckles\Camel\Extraction\ExtractedEndpointData;
 use Knuckles\Scribe\Tools\DocumentationConfig;
@@ -16,7 +16,7 @@ uses(TestCase::class);
 test('foo', function (): void {
     $route = new Route('post', '/api/posts', fn (CreateLaravelData $dataStub) => response()->noContent());
 
-    $example = new Plugin(new DocumentationConfig);
+    $example = new LaravelDataBodyParam(new DocumentationConfig);
 
     $result = $example(ExtractedEndpointData::fromRoute($route));
 
