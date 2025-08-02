a0ce17fa9f3a7f76b30bf6fd867b22d1911bb75c
Denis <korbakovd@gmail.com>
2025-08-03 02:07:46 +0300

    feat: create class for detected parent class

---
+++ src/Resolvers/ParentClassResolver.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DenisKorbakov\LaravelDataScribe\Resolvers;
+
+final readonly class ParentClassResolver
+{
+    public function __construct(
+        public string $className,
+    ){
+    }
+
+    public function parentClassName(): string
+    {
+        return get_parent_class($this->className) ?? "";
+    }
+
+    public function isInstanceOf(string $className): bool
+    {
+        return is_a($className, $this->parentClassName(), true);
+    }
+}
