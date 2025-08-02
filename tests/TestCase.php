272f5076b7ba68a7453f70f7eddda59ec027a09c
Denis <korbakovd@gmail.com>
2025-08-03 02:07:21 +0300

    feat: add provider for generate fake data for laravel data

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Spatie\LaravelData\LaravelDataServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function getPackageProviders($app): array
+    {
+        return [LaravelDataServiceProvider::class];
+    }
 }
