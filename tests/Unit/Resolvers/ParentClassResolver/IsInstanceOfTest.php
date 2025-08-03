d30c42299bb1017efedcd19d64da9fc64e41155e
Denis <korbakovd@gmail.com>
2025-08-03 15:15:53 +0300

    feat: create tests for parent resolver

--- tests/Unit/Resolvers/ParentClassResolver/IsInstanceOfTest.php
+++ tests/Unit/Resolvers/ParentClassResolver/IsInstanceOfTest.php
@@ -1,3 +1,24 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\LaravelDataScribe\Resolvers\ParentClassResolver;
+use Spatie\LaravelData\Data;
+use Tests\Fixtures\ParentClasses\ParentClassLaravelData;
+use Tests\Fixtures\ParentClasses\WithoutParentClass;
+
+test('is instance of Laravel data', function () {
+    $parentClassResolver = new ParentClassResolver(ParentClassLaravelData::class);
+
+    $result = $parentClassResolver->isInstanceOf(Data::class);
+
+    expect($result)->toBeTrue();
+});
+
+test('is not instance of Laravel data', function () {
+    $parentClassResolver = new ParentClassResolver(WithoutParentClass::class);
+
+    $result = $parentClassResolver->isInstanceOf(Data::class);
+
+    expect($result)->toBeFalse();
+});
--- tests/Unit/Resolvers/ParentClassResolver/ParentClassNameTest.php
+++ tests/Unit/Resolvers/ParentClassResolver/ParentClassNameTest.php
@@ -1,3 +1,25 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\LaravelDataScribe\Resolvers\ParentClassResolver;
+use Spatie\LaravelData\Data;
+use Tests\Fixtures\ParentClasses\ParentClassLaravelData;
+use Tests\Fixtures\ParentClasses\WithoutParentClass;
+
+test('get parent class name', function () {
+    $parentClassResolver = new ParentClassResolver(ParentClassLaravelData::class);
+
+    $result = $parentClassResolver->parentClassName();
+
+    expect($result)->toBe(Data::class);
+});
+
+test('get empty string', function () {
+    $parentClassResolver = new ParentClassResolver(WithoutParentClass::class);
+
+    $result = $parentClassResolver->parentClassName();
+
+    expect($result)->toBe('');
+});
+
