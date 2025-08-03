14e3d565cf8578957eb7d6f9baec3fa21fcb3a5f
Denis <korbakovd@gmail.com>
2025-08-03 18:42:20 +0300

    tests: add test cases

--- tests/Unit/Rules/ValidationRules/RulesTest.php
+++ tests/Unit/Rules/ValidationRules/RulesTest.php
@@ -1,3 +1,48 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\LaravelDataScribe\Rules\ValidationRules;
+use Tests\Fixtures\LaravelData\AttributeRules;
+use Tests\Fixtures\LaravelData\CustomAttributeRules;
+use Tests\Fixtures\LaravelData\ManualRules;
+use Tests\Fixtures\LaravelData\NoRules;
+use Tests\Fixtures\ParentClasses\WithoutParentClassLaravelData;
+
+mutates(ValidationRules::class);
+
+test('get validation rules from AttributeRules', function () {
+    $validationRules = new ValidationRules(AttributeRules::class);
+    $result = $validationRules->rules();
+
+    expect($result)->toEqual(AttributeRules::getValidationRules([]));
+});
+
+test('get validation rules from CustomAttributeRules', function () {
+    $validationRules = new ValidationRules(CustomAttributeRules::class);
+    $result = $validationRules->rules();
+
+    expect($result)->toEqual(CustomAttributeRules::getValidationRules([]));
+});
+
+test('get validation rules from ManualRules', function () {
+    $validationRules = new ValidationRules(ManualRules::class);
+    $result = $validationRules->rules();
+
+    expect($result)->toEqual(ManualRules::getValidationRules([]));
+});
+
+test('get validation rules from NoRules', function () {
+    $validationRules = new ValidationRules(NoRules::class);
+    $result = $validationRules->rules();
+
+    expect($result)->toEqual(NoRules::getValidationRules([]));
+});
+
+test('get validation rules from WithoutParentClassLaravelData', function () {
+    $validationRules = new ValidationRules(WithoutParentClassLaravelData::class);
+    $result = $validationRules->rules();
+
+    expect($result)->toEqual([]);
+});
+
