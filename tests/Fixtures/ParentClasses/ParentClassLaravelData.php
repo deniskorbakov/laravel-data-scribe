6dcfc91c1b8ce156f2e4f099dae351b83a7da800
Denis <korbakovd@gmail.com>
2025-08-03 15:15:31 +0300

    feat: create fixtures

---
+++ tests/Fixtures/ParentClasses/ParentClassLaravelData.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\ParentClasses;
+
+use Spatie\LaravelData\Data;
+
+final class ParentClassLaravelData extends Data
+{
+
+}
---
+++ tests/Fixtures/ParentClasses/WithoutParentClass.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\ParentClasses;
+
+final class WithoutParentClass
+{
+
+}
