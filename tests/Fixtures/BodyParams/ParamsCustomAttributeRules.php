213fc56325dc5dfa1fb8731f6bbaef5d91ebaa10
Denis <korbakovd@gmail.com>
2025-08-03 20:23:39 +0300

    feat: create fixtures for body params

---
+++ tests/Fixtures/BodyParams/ParamsCustomAttributeRules.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'phone' => [
+        'name' => 'phone',
+        'required' => true,
+        'type' => 'string',
+        'description' => '',
+        'nullable' => false,
+    ],
+    'password' => [
+        'name' => 'password',
+        'required' => true,
+        'type' => 'string',
+        'description' => '',
+        'nullable' => false,
+    ],
+];
---
+++ tests/Fixtures/BodyParams/ParamsManualRules.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name'               => [
+        'name'        => 'name',
+        'required'    => true,
+        'type'        => 'string',
+        'description' => 'Must not be greater than 100 characters.',
+        'nullable'    => false,
+    ],
+    'description'        => [
+        'name'        => 'description',
+        'required'    => true,
+        'type'        => 'string',
+        'description' => 'Must not be greater than 500 characters.',
+        'nullable'    => false,
+    ],
+    'price'              => [
+        'name'        => 'price',
+        'required'    => true,
+        'type'        => 'number',
+        'description' => 'Must be at least 0. Must not be greater than 9999.99.',
+        'nullable'    => false,
+    ],
+    'items'              => [
+        'name'        => 'items',
+        'required'    => true,
+        'type'        => 'object[]',
+        'description' => 'Must have at least 1 items.',
+        'nullable'    => false,
+    ],
+    'items[].product_id' => [
+        'name'        => 'items[].product_id',
+        'required'    => true,
+        'type'        => 'integer',
+        'description' => 'The <code>id</code> of an existing record in the products table.',
+        'nullable'    => false,
+    ],
+    'items[].quantity'   => [
+        'name'        => 'items[].quantity',
+        'required'    => true,
+        'type'        => 'integer',
+        'description' => 'Must be at least 1. Must not be greater than 10.',
+        'nullable'    => false,
+    ],
+];
