e8264321f84c9e3a098a4e7a5700564b35f6874c
Denis <korbakovd@gmail.com>
2025-08-03 14:28:34 +0300

    test: generate base test

--- tests/Unit/LaravelDataBodyParamTest.php
+++ tests/Unit/LaravelDataBodyParamTest.php
@@ -1,3 +1,19 @@
 <?php
 
 declare(strict_types=1);
+
+use DenisKorbakov\LaravelDataScribe\LaravelDataBodyParam;
+use Illuminate\Routing\Route;
+use Knuckles\Camel\Extraction\ExtractedEndpointData;
+use Knuckles\Scribe\Tools\DocumentationConfig;
+use Tests\Fixtures\LaravelData\CreateLaravelData;
+
+test('success - generate body params from laravel/data', function (): void {
+    $route = new Route('post', '/api/posts', fn (CreateLaravelData $dataStub) => response()->noContent());
+
+    $example = new LaravelDataBodyParam(new DocumentationConfig);
+
+    $result = $example(ExtractedEndpointData::fromRoute($route));
+
+    expect($result)->toBeArray();
+});
